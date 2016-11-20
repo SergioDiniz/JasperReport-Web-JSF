dda1a655f593051c1ca54469e242969b5a387547
Sergio Diniz <sergiodinizsh@gmail.com>
2016-11-20 15:43:34 -0300

    generating PDF

--- src/br/com/managedbeans/FuncionarioController.java
+++ src/br/com/managedbeans/FuncionarioController.java
@@ -1,16 +1,27 @@
 package br.com.managedbeans;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 
 import br.com.beans.Funcionario;
 import br.com.sessionbeans.FuncionarioIT;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 @ManagedBean
 @ViewScoped
@@ -18,6 +29,7 @@ public class FuncionarioController implements Serializable {
 
 	private Funcionario funcionario;
 	private List<Funcionario> funcionarios;
+	private JasperPrint jasperPrint;
 	
 	@EJB
 	private FuncionarioIT dao;
@@ -39,14 +51,26 @@ public class FuncionarioController implements Serializable {
 		this.funcionarios = dao.listFuncionarios();
 	}
 	
+	public void initReport() throws JRException{
+		JRBeanCollectionDataSource source = new JRBeanCollectionDataSource(this.funcionarios);
+		String realPath = FacesContext.getCurrentInstance().getExternalContext().getRealPath("/");
+		System.out.println(realPath);
+		String path = realPath + "WEB-INF/reports/funcionario.jasper";
+		jasperPrint = JasperFillManager.fillReport(path, null, source);
+	}
 	
+	public void gerarPDF() throws JRException, IOException{
+		initReport();
+		HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+		response.reset();
+		response.setContentType("application/pdf");
+		response.setHeader("Content-disposition", "attachment; filename=\"relatorio.pdf\"");
+		ServletOutputStream stream = response.getOutputStream();
+		JasperExportManager.exportReportToPdfStream(jasperPrint, stream);
+		FacesContext.getCurrentInstance().responseComplete();
+	}
 	
-	
-	
-	
-	
-	
-	
+
 
 	public Funcionario getFuncionario() {
 		return funcionario;
