3f7c66f567c91b3272687c52bcf944fffc6e114b
Sergio Diniz <sergiodinizsh@gmail.com>
2016-11-19 22:35:24 -0300

    first controller showing message in view

---
+++ src/br/com/controllers/OlaMundoMBean.java
@@ -0,0 +1,13 @@
+package br.com.controllers;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class OlaMundoMBean {
+
+	public String getMensagem() {
+		return "Ola Mundo Managed Bean";
+	}
+
+	
+}
--- src/br/com/dao/FuncionarioDAO.java
+++ src/br/com/dao/FuncionarioDAO.java
@@ -1,7 +1,11 @@
 package br.com.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import br.com.beans.Funcionario;
 
@@ -21,6 +25,21 @@ public class FuncionarioDAO {
 		} finally {
 			em.close();
 		}
+	}
+	
+	public List<Funcionario> listFuncionarios(){
+		List<Funcionario> funcionarios = new ArrayList<>();
+		
+		try {
+			
+			Query query = em.createQuery("from Funcionario");
+			funcionarios = query.getResultList();
+			
+		} finally {
+			em.close();
+		}
+		
+		return funcionarios;
 	}
 	
 }
